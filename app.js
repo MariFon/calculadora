85555c03c67fb593769192708b65d592d7a67ed3
MariFon <87157578+MariFon@users.noreply.github.com>
2021-09-28 22:18:01 -0300

    site calculadora
    não finalizado

---
+++ app.js
@@ -0,0 +1,24 @@
+// alert("Olá mundo")
+// console.log("Olá mundo")
+// console.log("Aprendendo javascript")
+// console.log(10+20)
+// console.log(10-3)
+// console.log(3*5)
+// console.log(20/4)
+// let nome="Guto"
+// let idade= 35
+// console.log("Olá" + nome)
+// console.log(idade -5)
+
+let nomeDoAluno= "Guto"
+let notaDoPrimeiroBimestre= 9
+let notaDoSegundoBimestre= 7
+let notaDoTerceiroBimestre= 4
+let notaDoQuartoBimestre= 2
+let media= (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) /4
+let mediaFixada= media.toFixed(1) 
+
+
+console.log("Boas vindas " + nomeDoAluno)
+console.log("Media")
+console.log(mediaFixada)
